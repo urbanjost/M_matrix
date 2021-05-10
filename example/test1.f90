bb86bce2f7637bcb264d737ec2e6a9e2def17fcc
John S. Urban <urbanjost@comcast.net>
2021-05-09 23:53:07 -0400

    incremental changes

--- example/test1.f90
+++ example/test1.f90
@@ -3,6 +3,6 @@ use m_matrix, only : mat88
    call mat88(4000) ! quiet initialization
    call mat88( 'a=<1 2 3 4; 5 6 7 8>;quit')
    call mat88( 'save("file1");quit')
-   call mat88( 'help INTRO;quit')
+   call mat88( 'help;quit')
    call mat88( ' ')
 end program bigmat
