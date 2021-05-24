a383ab932b2444a3449ebe70e1218c5c444f7118
John S. Urban <urbanjost@comcast.net>
2021-05-24 00:18:05 -0400

    rename examples

--- example/single_lines.f90
+++ example/single_lines.f90
@@ -1,6 +1,6 @@
 program bigmat
 use m_matrix, only : laff
-   call laff(4000) ! quiet initialization
+   call laff(4000) ! small memory footprint initialization
    call laff( 'a=<1 2 3 4; 5 6 7 8>;quit')
    call laff( 'save("file1");quit')
    call laff( 'help;quit')
